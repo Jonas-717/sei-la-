3e4890bdda21455b29049b7a30aaf6dc20d9b152
Jonas-717 <clajon854@gmail.com>
2025-09-21 10:14:00 -0300

    atualização 2

--- teste.py
+++ teste.py
@@ -1,6 +1,7 @@
 nota1 = float(input("digite a primeira nota:"))
 nota2 = float(input("digite a segunda nota:"))
-média = (nota1 + nota2) / 2
+nota3 = float(input("digite a terceira nota"))
+média = (nota1 + nota2 + nota3) / 3
 frequencia = int(input("digite a frequencia (%):"))
 
 if média < 6:
