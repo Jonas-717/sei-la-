2e96874e1ba233a8ee14c60e1bcecf11e4659d07
Jonas-717 <clajon854@gmail.com>
2025-09-20 22:09:29 -0300

    eu fiz um calculo pedagogico básico

--- teste.py
+++ teste.py
@@ -1,1 +1,13 @@
-print ("RECEBA")
+nota1 = float(input("digite a primeira nota:"))
+nota2 = float(input("digite a segunda nota:"))
+média = (nota1 + nota2) / 2
+frequencia = int(input("digite a frequencia (%):"))
+
+if média < 6:
+    print("ALUNO REPROVOU POR NOTA")
+else:
+    print("ALUNO PASSOU EM NOTA")
+if frequencia < 50:
+    print("ALUNO REPROVOU POR FALTA")
+else:
+    print("ALUNO PASSOU EM PRESENÇA")
